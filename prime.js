0789f339bd5a765ba523939409fc1b95784f6e26
Abhishek Pandey <123861746+abhi759724@users.noreply.github.com>
2023-02-21 23:51:47 +0530

    comment

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 let x = 11;
-let f = 0;
+let factors = 0;
 for(let i=0;i<=x;i++){
 if(x%i==0){
-f++
+factors++
 }
 }
-if(f==2){
+if(factors==2){
 console.log("prime")
 }else{
 console.log("Not prime")
